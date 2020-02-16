53d4137a35e225a86128f65131caa4591580832e
Sergey <snugryumov@gmail.com>
2020-02-16 20:21:11 +0300

    feature: adaptive

--- client/components/Intro/Intro.tsx
+++ client/components/Intro/Intro.tsx
@@ -11,11 +11,7 @@ function Intro(): ReactElement {
           delectus laborum quaerat hic, officiis nobis repellendus? Temporibus accusamus ab
           voluptatem rem totam aliquam nihil sit ipsum sequi, facilis adipisci dolor qui quidem
           mollitia nisi, odio perferendis enim minima ex id ratione voluptates sed. Nostrum
-          exercitationem a dolorum natus veritatis, nobis totam eum expedita modi commodi qui autem,
-          sapiente tempora eveniet placeat fugit enim ea atque odit, iure eius id deserunt ratione.
-          Possimus neque perferendis recusandae sint rem sapiente. Vero, ea? Magnam est minima
-          corrupti perspiciatis ipsa hic, at quae recusandae ad error quas molestias cumque
-          exercitationem possimus.
+          exercitationem a dolorum natus veritatis, nobis totam eum expedita modi commodi qui autem.
         </p>
       </div>
     </div>
