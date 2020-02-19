b234e99942e9034b60dbd5da2d523620a84a728d
suslovsd@lad24.ru <suslovsd@lad24.ru>
2020-02-19 17:00:02 +0300

    refactor: Filter - добавил адаптив для фильтра

--- client/components/Select/Select.tsx
+++ client/components/Select/Select.tsx
@@ -2,22 +2,47 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import SimpleSelect from '@material-ui/core/Select';
-import { makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import React, { ReactElement } from 'react';
-import './Select.scss';
 
 interface Props {
   setLimitCount(value: number): void;
 }
 
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
+const theme = createMuiTheme({
+  overrides: {
+    MuiFormControl: {
+      root: {
+        width: '100%',
+      },
+    },
+    MuiInput: {
+      underline: {
+        '&::before': {
+          borderBottom: '1px solid #777',
+        },
+        '&::after': {
+          borderBottom: '2px solid #e05927',
+        },
+      },
+    },
+    MuiSelect: {
+      select: {
+        padding: '10px',
+      },
+    },
+    MuiInputLabel: {
+      root: {
+        '&$focused': {
+          color: '#111',
+        },
+      },
+    },
   },
 });
 
 function Select(props: Props): ReactElement {
-  const classes = useStyles();
   const [age, setAge] = React.useState('');
 
   const handleChange = (event: any) => {
@@ -25,18 +50,16 @@ function Select(props: Props): ReactElement {
     setAge(event.target.value);
   };
   return (
-    <FormControl
-      classes={{
-        root: classes.root,
-      }}
-    >
-      <InputLabel id="select-label">Показывать по</InputLabel>
-      <SimpleSelect labelId="select-label" id="simple-select" value={age} onChange={handleChange}>
-        <MenuItem value={2}>2</MenuItem>
-        <MenuItem value={5}>5</MenuItem>
-        <MenuItem value={10}>10</MenuItem>
-      </SimpleSelect>
-    </FormControl>
+    <ThemeProvider theme={theme}>
+      <FormControl>
+        <InputLabel id="select-label">Показывать по</InputLabel>
+        <SimpleSelect labelId="select-label" id="simple-select" value={age} onChange={handleChange}>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={10}>10</MenuItem>
+        </SimpleSelect>
+      </FormControl>
+    </ThemeProvider>
   );
 }
 
