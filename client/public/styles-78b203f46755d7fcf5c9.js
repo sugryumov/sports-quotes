55c5b541a207a2aab43f12295a5cd2d0c28b4477
suslovsd@lad24.ru <suslovsd@lad24.ru>
2020-03-04 15:54:21 +0300

    убрал public из gitignore

---
+++ client/public/styles-78b203f46755d7fcf5c9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"+PXe":function(n,o,c){},"+V0B":function(n,o,c){},"+asA":function(n,o,c){},"3JNx":function(n,o,c){},"5Tf3":function(n,o,c){},"6ac0":function(n,o,c){},ElSq:function(n,o,c){},GDTY:function(n,o,c){},SGS6:function(n,o,c){},Vklm:function(n,o,c){},aJrS:function(n,o,c){},ai9x:function(n,o,c){},fRaX:function(n,o,c){},pb6S:function(n,o,c){},uyya:function(n,o,c){},"v+pe":function(n,o,c){}}]);
+//# sourceMappingURL=styles-78b203f46755d7fcf5c9.js.map
