a8d9174c3af9ee7c322815278b636e7cace25c97
suslovsd@lad24.ru <suslovsd@lad24.ru>
2020-03-10 11:28:57 +0300

    feature: добавил вывод рандомной цитаты и поправил css для кнопок

--- client/src/components/Intro/Intro.tsx
+++ client/src/components/Intro/Intro.tsx
@@ -1,30 +1,29 @@
 import React, { ReactElement, useEffect, useState } from 'react';
 import './Intro.css';
 import Button from '../../components/Button/Button';
-// import { getQuotesForId } from '../../helpers/services';
+import { getRandomQuote } from '../../helpers/services';
 
 function Intro(): ReactElement {
-  const [quote, setQuote] = useState({
-    author: 'Серега Татанов',
-    quote:
-      '- Как то раз покурил перед стартом, и сразу всех обогнал.С тех пор не изменяю этой традиции',
-  });
+  const [randomQuote, setRandomQuote] = useState<{
+    quote: string;
+    author: string;
+    category: string;
+  }>({ quote: '', author: '', category: '' });
 
-  // useEffect(() => {
-  //   getQuotesForId(randomNumber)
-  //     .then(res => {
-  //       console.log(res, 'res');
-  //       setQuote(res.data.quote);
-  //     })
-  //     .catch(err => console.log(err));
-  // }, []);
+  useEffect(() => {
+    getRandomQuote()
+      .then(res => {
+        setRandomQuote(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <div className="intro container">
       <h1 className="intro__title">Цитаты спортсменов</h1>
-      <p className="intro__text">{quote.quote}</p>
-      <h3 className="intro__author">{quote.author}</h3>
-      <Button func={() => {}} text={'Предложить цитату'} color={'#fff'} bg={'#E05927'} />
+      <p className="intro__text">{randomQuote.quote}</p>
+      <h3 className="intro__author">{randomQuote.author}</h3>
+      <Button func={() => {}} text={'Предложить цитату'} type={'secondary'} />
     </div>
   );
 }
